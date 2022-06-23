9eb312e726722d314da9450ab45835e1b2801c93
onyinyeokwuosa <o.okwuosa34@gmail.com>
2022-06-22 20:03:46 -0400

    downloaded react router and began creating routes

--- couplesapp/src/App.js
+++ couplesapp/src/App.js
@@ -1,19 +1,30 @@
 import './App.css';
 import Header from './Header';
+// Running on Router v5.2 in order to use switch component 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
-
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>I am the chat page</h1>
+          </Route>
+          <Route path="/">
+            <h1>I am homepage</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+      
       {/*  Cards  */}
       {/*  Buttons below cards  */}
 
       {/*  Chats screen  */}
       {/*  Individual chat screen  */}
-
-    </div>
-  );
-}
 
 export default App;
