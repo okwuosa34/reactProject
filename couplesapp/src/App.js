785dc2135413a964efaf1ae68f8fc7177fd44047
onyinyeokwuosa <o.okwuosa34@gmail.com>
2022-06-29 19:44:50 -0400

    .

--- couplesapp/src/App.js
+++ couplesapp/src/App.js
@@ -4,17 +4,24 @@ import Header from './Header';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import CouplesCards from './components/CouplesCards';
 import SwipeButtons from './components/SwipeButtons';
+import Chats from './components/Chats';
+import ChatScreen from './components/ChatScreen';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Router>
         <Switch>
+          <Route path="/chat/:person">
+            <Header backButton="/chat" />
+            <ChatScreen />
+          </Route>
           <Route path="/chat">
-            <h1>I am the chat page</h1>
+            <Header backButton="/" />
+            <Chats />
           </Route>
           <Route path="/">
+            <Header />
             <CouplesCards />
             <SwipeButtons />
           </Route>
@@ -24,9 +31,5 @@ function App() {
   );
 }
       
-      {/*  Buttons below cards  */}
-
-      {/*  Chats screen  */}
-      {/*  Individual chat screen  */}
 
 export default App;
--- couplesapp/src/Header.js
+++ couplesapp/src/Header.js
@@ -8,26 +8,43 @@ import ChatIcon from '@mui/icons-material/Chat';
 import { brown } from '@mui/material/colors';
 // Importing iconButton click feature
 import IconButton from '@mui/material/IconButton';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { Link, useHistory } from 'react-router-dom';
+import Logo from "./AltLogo.png";
 
-function Header() {
+function Header({ backButton }) {
+  // Provides session history 
+  const history = useHistory();
   return (
       // BEM, Material UI was used for icons
+      // history.replace(backButton) is replacing history with url
     <div className="header">
-        <IconButton>
-        <PersonIcon sx={{ color: brown[200] }} className="header__icon" fontSize="large" />
+      {backButton ? (
+        <IconButton onClick={() => history.replace(backButton)}>
+          <ArrowBackIosIcon sx={{ color: brown[200] }} fontSize="large" className="header__icon" />
         </IconButton>
+      ) : (
+        <IconButton>
+          <PersonIcon sx={{ color: brown[200] }} className="header__icon" fontSize="large" />
+        </IconButton>
+      )}
 
+      <Link to="/">
         <img 
         className="header__logo"
-        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-emblem.jpg" 
+        src={Logo} 
         alt="couple logo"
         />
+      </Link>
 
-        <IconButton>
+      <Link to="/chat">
+      <IconButton>
         <ChatIcon sx={{ color: brown[200] }} className="header__icon" fontSize="large" />
-        </IconButton>      
+      </IconButton>
+      </Link>
+              
     </div>
   );
 }
 
-export default Header
+export default Header;
