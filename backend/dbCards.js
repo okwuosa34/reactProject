d0d2d8015baeb862d8f679243bf9d6f5debcd15f
onyinyeokwuosa <o.okwuosa34@gmail.com>
2022-07-11 21:42:40 -0400

    creating app config and endpoints

---
+++ backend/dbCards.js
@@ -0,0 +1,12 @@
+// Outlines the structure of the cards in the database
+
+import mongoose from 'mongoose'
+
+// schema to build database fields
+const cardSchema = mongoose.Schema({
+    name: String,
+    imgUrl: String
+})
+
+// With noSQL dbs collections are used instead of tables
+export default mongoose.model('cards', cardSchema);
