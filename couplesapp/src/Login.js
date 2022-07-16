25f6a432bea167e8388a67d7d46035b170a94918
onyinyeokwuosa <o.okwuosa34@gmail.com>
2022-07-16 12:43:16 -0400

    created account box completed nav bar

--- couplesapp/src/Login.js
+++ couplesapp/src/Login.js
@@ -1,31 +1,39 @@
-import React from 'react'
-import './Login.css';
+import React, { useState } from "react";
+import "./Login.css";
 import LoginNavbar from "./components/LoginNavbar";
+import AccountBox from "./components/AccountBox";
 
 function Login() {
-  
-  const authToken = false
+  // When button is clicked (true) box login / account creation pops up
+  const [showBox, setShowBox] = useState(false);
+
+  const authToken = false;
   const handleClick = () => {
-      console.log('clicked')
-  } 
+    console.log("clicked");
+    setShowBox(true);
+  };
 
+  // authToken if true will allow users to sign out, if false will allow users to create account
 
-  /* authToken if true will allow users to sign out, if false 
-  will allow users to create account */
-  
+  // for lines 30-32 if showBox is true then AccountBox will show
+  // line 31 calling setState from another component
+
   return (
-    <div className="cover">  
-    <div className="loginBody"> 
-    <LoginNavbar authToken={authToken} />
-    <div className="login">
-        <h1>Couples Connect©</h1>
-        <button className="loginButton" onClick={handleClick}>
-            {authToken ? 'Signout' : 'Create Account'}
-        </button>
+    <div className="cover">
+      <div className="loginBody">
+        <LoginNavbar authToken={authToken} setShowBox={setShowBox} showBox={showBox} />
+        <div className="login">
+          <h1>Couples Connect©</h1>
+          <button className="loginButton" onClick={handleClick}>
+            {authToken ? "Signout" : "Create Account"}
+          </button>
+          <div className="accountBox">
+              {showBox && <AccountBox setShowBox={setShowBox} />}
+         </div>
+        </div>
+      </div>
     </div>
-    </div>
-    </div> 
-  )
+  );
 }
 
-export default Login
+export default Login;
