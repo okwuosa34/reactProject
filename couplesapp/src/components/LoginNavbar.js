6bfaf35daf7b60ddaa8ab6226f19427ee54e5213
onyinyeokwuosa <o.okwuosa34@gmail.com>
2022-07-17 17:32:18 -0400

    user profile creastion along with styling

--- couplesapp/src/components/LoginNavbar.js
+++ couplesapp/src/components/LoginNavbar.js
@@ -6,13 +6,14 @@ import './LoginNavbar.css';
 // if authToken does exist log in button will render to screen
 // handlClick function allows for showBox to be accessed from create account button as well as login button
 // Line 27 showBox variable will decide disabled feature
-function LoginNavbar({ authToken, setShowBox, showBox, setSigningUp }) {
+function LoginNavbar({ setShowBox, showBox, setSigningUp }) {
 
   const handleClick = () => {
     setShowBox(true)
     setSigningUp(false)
   }
 
+  const authToken = false
   return (
     <nav> 
         <div className="navLogo">
